70b9e94b976c8a50dd529441360edbef41eccf32
facundo brondo <facundobrondo34@gmail.com>
2022-11-22 17:41:49 -0300

    plantllas

---
+++ main.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const Products = require('./products/products')
+const { Router } = express
+const router = Router()
+const products = new Products(__dirname + '/data/products.json')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({
+    extended : true
+}))
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    return res.render('ejs/form')
+})
+app.get('/list', (req, res) =>{
+    return res.render('ejs/list',{
+        list: products.list
+    })
+})
+router.post('/',(req, res) =>{
+    const obj = req.body
+    products.insert(obj)
+    return res.redirect('/')
+})
+app.use('/api/productos', router)
+app.listen(8080)
